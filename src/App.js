23db8a1e9a7bf0a2ea502089f489e5edc68247a8
Yash Ladlapure <yash.ladlapure@gmail.com>
2026-03-27 01:47:49 +0530

    Add src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import DataForm from './components/DataForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FSD Lab 5 - React</h1>
+      <DataForm />
+    </div>
+  );
+}
+
+export default App;
