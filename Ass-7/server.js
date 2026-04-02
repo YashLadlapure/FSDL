6b3d6ef5876dc2b74ba7522ded6aea0d7154bd2f
Yash Ladlapure <yash.ladlapure@gmail.com>
2026-04-02 23:44:13 +0530

    Add files via upload

---
+++ Ass-7/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+
+
+const app = express()
+
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+
+app.use('/api/workouts', workoutRoutes)
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
